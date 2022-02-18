57c84d2731d3c4cb3b55e015838e0f023b5a4ad5
Carlos Serrano <carlosserranoquerol@hotmail.com>
2022-02-18 15:25:08 +0100

    incluidos los repositorios de la capa de dominio (#3)
    Co-authored-by: Carlos Serrano <carlos.serrano@isolutions.ch>

---
+++ CursoArquitecturaNet.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CursoArquitecturaNet.Core.Entities;
+using CursoArquitecturaNet.Core.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Core.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetProductListAsync();
+        Task<IEnumerable<Product>> GetProductByNameAsync(string productName);
+        Task<IEnumerable<Product>> GetProductByCategoryAsync(int categoryId);
+    }
+}
