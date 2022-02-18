fd025fbee5e7adb7c64db756e2a251c165cf62ed
Carlos Serrano <carlosserranoquerol@hotmail.com>
2022-02-18 14:46:22 +0100

    Entidades de la capa de dominio (#2)
    Co-authored-by: Carlos Serrano <carlos.serrano@isolutions.ch>

---
+++ CursoArquitecturaNet.Core/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using CursoArquitecturaNet.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }        
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }                
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public static Product Create(int productId, int categoryId, string name, decimal? unitPrice = null,
+                                     short? unitsInStock = null, short? unitsOnOrder = null, short? reorderLevel = null,
+                                     bool discontinued = false)
+        {
+            var product = new Product
+            {
+                Id = productId,
+                CategoryId = categoryId,
+                ProductName = name,
+                UnitPrice = unitPrice,
+                UnitsInStock = unitsInStock,
+                UnitsOnOrder = unitsOnOrder,                
+            };
+            return product;
+        }
+    }
+}
