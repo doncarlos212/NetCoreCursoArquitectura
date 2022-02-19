0d92b6b5024ef563b3130bede69978c26c4685f0
Carlos Serrano <carlosserranoquerol@hotmail.com>
2022-02-19 11:50:04 +0100

    añadidos los repositorios de la capa de infraestructura. (#7)
    Co-authored-by: Carlos Serrano <carlos.serrano@isolutions.ch>

--- CursoArquitecturaNet.Core/Repositories/Base/IRepository.cs
+++ CursoArquitecturaNet.Core/Repositories/Base/IRepository.cs
@@ -12,8 +12,7 @@ namespace CursoArquitecturaNet.Core.Repositories.Base
     public interface IRepository<T> where T: BaseEntity
     {
         Task<IReadOnlyList<T>> GetAllAsync();
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);        
-        Task<IReadOnlyList<T>> GetAsync(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);               
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
---
+++ CursoArquitecturaNet.Infraestructure/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using CursoArquitecturaNet.Core.Entities;
+using CursoArquitecturaNet.Core.Repositories;
+using CursoArquitecturaNet.Infraestructure.Data;
+using CursoArquitecturaNet.Infraestructure.Repository.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoArquitecturaNet.Infraestructure.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(CursoArquitecturaNetContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategoryAsync(int categoryId)
+        {
+            return await dbContext.Products
+                 .Where(x => x.CategoryId == categoryId)
+                 .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string productName)
+        {
+            return await dbContext.Products
+                .Where(x => x.ProductName.Contains(productName))
+                .Include(x => x.Category)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductListAsync()
+        {
+            return await GetAllAsync(); //Ver si necesito los includes.
+        }
+    }
+}
